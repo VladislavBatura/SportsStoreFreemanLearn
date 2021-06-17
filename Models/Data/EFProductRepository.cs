11736024dd104f625f7e3913caae75fc9997a9da
Aviat0r <ktulhuvas@gmail.com>
2021-06-17 21:10:17 +0700

    Added EFCore support, replaced fake db with real, configuration's set

---
+++ Models/Data/EFProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models.Interfaces;
+
+namespace SportsStore.Models.Data
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,16 +9,22 @@ using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 using SportsStore.Models.Data;
 using SportsStore.Models.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>(); //Даёт возможность временно использовать фейковую бд
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                    Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>(); //Даёт возможность временно использовать фейковую бд UPD. Произошла замена
             services.AddMvc();
         }
 
@@ -30,7 +36,9 @@ namespace SportsStore
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
-
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Product}/{action=List}/{id?}");
             });
         }
     }
