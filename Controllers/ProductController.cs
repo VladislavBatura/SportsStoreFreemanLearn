fe3960b60bcf8ff3557bca4db49fbe98e7d30f60
Aviat0r <ktulhuvas@gmail.com>
2021-06-17 18:27:32 +0700

    Add controller for view of products

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Interfaces;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository; //внедряем зависимость для упрощения дальнейшей работы с бд
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List() => View(_repository.Products); //упрощение конструкции вызова представления
+
+    }
+}
